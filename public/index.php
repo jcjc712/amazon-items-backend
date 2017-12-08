f2fa778b0533811bb56c48623a16b8bb4ebed481
jcjc712 <jcjc712@hotmail.com>
2017-12-08 05:35:15 +0000

    feat middleware for cors

--- public/index.php
+++ public/index.php
@@ -20,9 +20,6 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 require __DIR__.'/../vendor/autoload.php';
 
 /*
